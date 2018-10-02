0355fcf8ff0fec6569e1589f6bce0fcc779e3ca2
Abhishek Thakur <abhi.una12@gmail.com>
2018-10-02 12:13:24 +0530

    Create frames2vid.py

---
+++ frames2vid.py
@@ -0,0 +1,40 @@
+import os, glob
+
+def make_video(outvid, images=None, fps=30, size=None,
+               is_color=True, format="FMP4"):
+    """
+    Create a video from a list of images.
+ 
+    @param      outvid      output video
+    @param      images      list of images to use in the video
+    @param      fps         frame per second
+    @param      size        size of each frame
+    @param      is_color    color
+    @param      format      see http://www.fourcc.org/codecs.php
+    @return                 see http://opencv-python-tutroals.readthedocs.org/en/latest/py_tutorials/py_gui/py_video_display/py_video_display.html
+    """
+    from cv2 import VideoWriter, VideoWriter_fourcc, imread, resize
+    fourcc = VideoWriter_fourcc(*format)
+    vid = None
+    for image in images:
+        if not os.path.exists(image):
+            raise FileNotFoundError(image)
+        img = imread(image)
+        if vid is None:
+            if size is None:
+                size = img.shape[1], img.shape[0]
+            vid = VideoWriter(outvid, fourcc, float(fps), size, is_color)
+        if size[0] != img.shape[1] and size[1] != img.shape[0]:
+            img = resize(img, size)
+        vid.write(img)
+    vid.release()
+    return vid
+
+VIDEO_SAVE_DIR = '/content/videos'
+images = list(glob.iglob(os.path.join(VIDEO_SAVE_DIR, '*.*')))
+# Sort the images by name index.
+images = sorted(images, key=lambda x: float(os.path.split(x)[1][:-3]))
+print(images)
+outvid = os.path.join(VIDEO_DIR, "output.mp4")
+make_video(outvid, images, fps=30)
+println("Done...!")
